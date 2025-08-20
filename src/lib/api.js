5a022c17c428acc1d4a12ebdb938619a0011a51d
sachinrrr <sachinrubera1@gmail.com>
2025-08-20 11:35:03 +0530

    add base url import function

--- src/lib/api.js
+++ src/lib/api.js
@@ -6,8 +6,8 @@
    export const Api = createApi({
      reducerPath: "Api",
      baseQuery: fetchBaseQuery({
-       // Hardcoded URL - will work immediately
-       baseUrl: "https://fed-2-back-end-sachin.onrender.com/api",
+       // Use environment variable instead of hardcoded localhost
+       baseUrl: `${import.meta.env.VITE_BASE_URL}/api`,
        prepareHeaders: async (headers) => {
          return new Promise((resolve) => {
            async function checkToken() {
